43c6e1da315c4f68bb10358c11c220bd29551f84
Greg Hurrell <greg@hurrell.net>
2015-07-25 00:36:49 -0700

    Opt out of Jest automocking for now
    I always end up turning off mocking, and creating manual mocks
    seems like a lot of overhead. Let's just opt out for now.

--- src/__mocks__/dedent.js
+++
@@ -1,8 +0,0 @@
-/**
- * Copyright 2015-present Greg Hurrell. All rights reserved.
- * Licensed under the terms of the MIT license.
- */
-
-'use strict';
-
-module.exports = require.requireActual('../dedent');
