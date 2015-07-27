aabeaecb7e2f3e33aca9527d9bd797db10589dc0
Greg Hurrell <greg@hurrell.net>
2015-07-26 20:29:17 -0700

    Add command-line options

--- src/run.js
+++ src/run.js
@@ -12,11 +12,31 @@ import fs from 'fs';
 import getFiles from './getFiles';
 import lex from './lex';
 import parse from './parse';
+import yargs from 'yargs';
 
 const readFile = Promise.promisify(fs.readFile);
 
 export default async function run(): void {
-  const files = await getFiles('.');
+  const argv = yargs
+    .version(() => require('../package').version)
+    .usage('Usage: $0 [options]')
+    .example('$0 -C build file.txt README.md -')
+    .alias('C', 'directory')
+    .default('C', '.', 'current directory')
+    .describe('C', 'Change to directory before processing')
+    .nargs('C', 1)
+    .string('C')
+    .help('h')
+    .alias('h', 'help')
+    .epilog('https://github.com/wincent/docvim')
+    .strict()
+    .argv;
+
+  const directory = Array.isArray(argv.directory) ?
+    argv.directory[argv.directory.length - 1] :
+    argv.directory;
+
+  const files = await getFiles(directory);
   const asts = await* files.map(async filename => {
     const contents = await readFile(filename);
     return parse(lex(contents.toString()));
