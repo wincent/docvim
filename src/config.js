6593cdefb8ebee1a43fe5568219fe694f6c077be
Greg Hurrell <greg@hurrell.net>
2015-07-27 16:00:31 -0700

    Beef up option parsing
    - fix ugly formatting of example by increasing the wrap width; no idea
      why it is wrapping so early otherwise
    - do some post-processing of the output files to unpack them into
      objects describing their type and location; log errors if there is a
      problem

--- src/config.js
+++ src/config.js
@@ -7,12 +7,14 @@
 
 'use strict';
 
+import path from 'path';
 import yargs from 'yargs';
 
 const json = require('../package');
 
-const config = yargs
+const options = yargs
   .usage('Usage: $0 [option...] [outfile...]')
+  .wrap(Math.min(yargs.terminalWidth(), 90))
   .example('$0 -C build file.txt README.md')
   .alias('C', 'directory')
   .default('C', '.', 'current directory')
@@ -26,11 +28,39 @@ const config = yargs
   .alias('h', 'help')
   .version(json.version)
   .epilog(json.homepage)
-  .strict()
-  .argv;
+  .strict();
+
+const config = options.argv;
 
 if (Array.isArray(config.directory)) {
   config.directory = config.directory[config.directory.length - 1];
+}
+
+config.output = config._;
+if (!config.output.length) {
+  // When no explicit outputs, we output in Vim help format to standard out.
+  config.output.push({format: 'help', destination: null});
+} else {
+  const errors = [];
+  config.output.forEach(output => {
+    switch (path.extname(output).toLowerCase()) {
+      case '.txt':
+        config.output.push({format: 'help', destination: output});
+        break;
+      case '.md':
+        config.output.push({format: 'markdown', destination: output});
+        break;
+      default:
+        errors.push(
+          'Bad filename `' + output + '`: a ".txt" or ".md" extension required'
+        );
+    }
+  });
+  if (errors.length) {
+    options.showHelp();
+    errors.forEach(error => console.error(error));
+    process.exit(1);
+  }
 }
 
 export default config;
