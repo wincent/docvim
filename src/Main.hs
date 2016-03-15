0404cf1bc0a4cfeb27ecc3b758cbc36a453f812e
Greg Hurrell <greg@hurrell.net>
2016-03-15 08:36:09 -0700

    Add starting point for options parsing
    Based on sample in docs at:
    
    https://hackage.haskell.org/package/optparse-applicative#readme
    
    I will go through and add more options in a later pass. This article has
    a nice overview of how things should be structured:
    
    https://robots.thoughtbot.com/applicative-options-parsing-in-haskell

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,33 @@
 -- | The docvim executable.
 module Main (main) where
 
+import Options.Applicative
+
+data Config = Config
+  { directory :: String
+  , debug :: Bool }
+
+config :: Parser Config
+config = Config
+     <$> strOption
+         ( long "directory"
+        <> short 'c'
+        <> metavar "DIRECTORY"
+        <> help "Change to DIRECTORY before processing" )
+     <*> switch
+         ( long "debug"
+        <> short 'd'
+        <> help "Print debug information during processing" )
+
+
+intro :: Config -> IO ()
+intro (Config d False) = putStrLn "debugging off"
+intro (Config d True) = putStrLn "debugging on"
+
 main :: IO ()
-main = putStrLn "docvim"
+main = execParser opts >>= intro
+  where
+    opts = info (helper <*> config)
+        (  fullDesc
+        <> progDesc "Generate documentation for a Vim plug-in"
+        <> header "docim - a documentation generator for Vim plug-ins" )
