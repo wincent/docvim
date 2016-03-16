2fc7e3bff7162ce05b28546fd63c89b3f0ede87a
Greg Hurrell <greg@hurrell.net>
2016-03-16 09:07:44 -0700

    Split options parsing into a separate file

--- src/Main.hs
+++ src/Main.hs
@@ -1,55 +1,7 @@
 -- | The docvim executable.
 module Main (main) where
 
-import Options.Applicative
-import Data.Version (showVersion)
-
-import qualified Paths_docvim (version)
-
-data Options = Options
-  { outfile :: Maybe FilePath
-  , debug :: Bool
-  , directory :: String
-  , verbose :: Bool }
-
-parseOutfile :: Parser String
-parseOutfile = argument str
-  (  metavar "OUTFILE"
-  <> help (unlines [ "Target file for generated output"
-                   , "(default: standard output)" ]))
-
-version :: Parser (a -> a)
-version = infoOption (showVersion Paths_docvim.version)
-  (  long "version"
-  <> help "Print version information" )
-
-parseDebug :: Parser Bool
-parseDebug = switch
-  $  long "debug"
-  <> short 'd'
-  <> help "Print debug information during processing"
-
-parseDirectory :: Parser String
-parseDirectory = strOption
-  $  long "directory"
-  <> short 'c'
-  <> metavar "DIRECTORY"
-  <> value "."
-  <> showDefault
-  <> help "Change to DIRECTORY before processing"
-
-parseVerbose :: Parser Bool
-parseVerbose = switch
-  $  long "verbose"
-  <> short 'v'
-  <> help "Be verbose during processing"
-
-parseOptions :: Parser Options
-parseOptions = Options
-  <$> optional parseOutfile
-  <*> parseDebug
-  <*> parseDirectory
-  <*> parseVerbose
+import Options (Options(..), options)
 
 run :: Options -> IO ()
 run (Options (Just a) _ _ _) = putStrLn ("got file " ++ a)
@@ -57,9 +9,4 @@ run (Options _ False d _) = putStrLn "debugging off"
 run (Options _ True d _) = putStrLn "debugging on"
 
 main :: IO ()
-main = execParser options >>= run
-  where
-    options = info (helper <*> version <*> parseOptions)
-            ( fullDesc
-            <> progDesc "Generate documentation for a Vim plug-in"
-            <> header "docvim - a documentation generator for Vim plug-ins" )
+main = options >>= run
---
+++ src/Options.hs
@@ -0,0 +1,57 @@
+-- | Options parser for the docvim executable.
+module Options (Options(..), options) where
+
+import Options.Applicative
+import Data.Version (showVersion)
+import qualified Paths_docvim (version)
+
+data Options = Options
+  { outfile :: Maybe FilePath
+  , debug :: Bool
+  , directory :: String
+  , verbose :: Bool }
+
+parseOutfile :: Parser String
+parseOutfile = argument str
+  (  metavar "OUTFILE"
+  <> help (unlines [ "Target file for generated output"
+                   , "(default: standard output)" ]))
+
+version :: Parser (a -> a)
+version = infoOption (showVersion Paths_docvim.version)
+  (  long "version"
+  <> help "Print version information" )
+
+parseDebug :: Parser Bool
+parseDebug = switch
+  $  long "debug"
+  <> short 'd'
+  <> help "Print debug information during processing"
+
+parseDirectory :: Parser String
+parseDirectory = strOption
+  $  long "directory"
+  <> short 'c'
+  <> metavar "DIRECTORY"
+  <> value "."
+  <> showDefault
+  <> help "Change to DIRECTORY before processing"
+
+parseVerbose :: Parser Bool
+parseVerbose = switch
+  $  long "verbose"
+  <> short 'v'
+  <> help "Be verbose during processing"
+
+parseOptions :: Parser Options
+parseOptions = Options
+  <$> optional parseOutfile
+  <*> parseDebug
+  <*> parseDirectory
+  <*> parseVerbose
+
+options :: IO Options
+options = execParser $ info (helper <*> version <*> parseOptions)
+  ( fullDesc
+  <> progDesc "Generate documentation for a Vim plug-in"
+  <> header "docvim - a documentation generator for Vim plug-ins" )
