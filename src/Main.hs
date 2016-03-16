173ba115a157a816c62512cf2e9eab090726275b
Greg Hurrell <greg@hurrell.net>
2016-03-16 07:05:01 -0700

    Add --version flag
    Google search for how to use `infoOption` led to this:
    
    https://github.com/fujimura/git-freq/blob/12d38a1f9109d307e420acc7facc85e425bb6349/src/Main.hs
    
    Cargo-culted it from there.

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,17 @@
 module Main (main) where
 
 import Options.Applicative
+import Data.Version (showVersion)
+
+import qualified Paths_docvim (version)
 
 data Options = Options
   { debug :: Bool
   , directory :: String }
+
+version = infoOption (showVersion Paths_docvim.version)
+  ( long "version"
+  <> help "Print version information" )
 
 parseDebug :: Parser Bool
 parseDebug = switch
@@ -26,7 +33,6 @@ parseOptions = Options
   <$> parseDebug
   <*> parseDirectory
 
-
 run :: Options -> IO ()
 run (Options False d) = putStrLn "debugging off"
 run (Options True d) = putStrLn "debugging on"
@@ -34,7 +40,7 @@ run (Options True d) = putStrLn "debugging on"
 main :: IO ()
 main = execParser options >>= run
   where
-    options = info (helper <*> parseOptions)
+    options = info (helper <*> version <*> parseOptions)
             ( fullDesc
             <> progDesc "Generate documentation for a Vim plug-in"
             <> header "docim - a documentation generator for Vim plug-ins" )
