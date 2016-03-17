cb183caef53a00f31f1d3965bf36ff8e98a0794f
Greg Hurrell <greg@hurrell.net>
2016-03-17 13:38:46 -0700

    Actually respect -c command-line option

--- src/Main.hs
+++ src/Main.hs
@@ -6,13 +6,10 @@ import ReadDir (readDir)
 import System.FilePath (takeExtension)
 
 run :: Options -> IO ()
-run _ = do
-  contents <- readDir "."
+run (Options _ _ directory _) = do
+  contents <- readDir directory
   let filtered = filter (\path -> takeExtension path == ".vim") contents
   putStrLn $ show filtered
-run (Options (Just a) _ _ _) = putStrLn ("got file " ++ a)
-run (Options _ False d _) = putStrLn "debugging off"
-run (Options _ True d _) = putStrLn "debugging on"
 
 main :: IO ()
 -- | Run the executable using the supplied options.
