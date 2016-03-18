be78d0d5a6e03edcdc96aa4dd5fc09d920f36a55
Greg Hurrell <greg@hurrell.net>
2016-03-17 23:02:37 -0700

    Slightly neaten conditional logging

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 -- | The docvim executable.
 module Main (main) where
 
+import Control.Monad (when)
 import Options (Options(..), options)
 import Parse (parse)
 import ReadDir (readDir)
@@ -11,8 +12,7 @@ run (Options _ _ directory verbose) = do
   paths <- readDir directory
   let filtered = filter (\path -> takeExtension path == ".vim") paths
   parsed <- mapM (\path -> do
-      if verbose then putStrLn $ "Processing " ++ path
-      else return ()
+      when verbose (putStrLn $ "Processing " ++ path)
       parse path
     ) filtered
   print parsed
