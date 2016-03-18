4fe68cfb5a7e9d2e58a6afe3bcc5ebbd00c2f06a
Greg Hurrell <greg@hurrell.net>
2016-03-17 22:46:49 -0700

    Split out Parse module

--- src/Main.hs
+++ src/Main.hs
@@ -1,71 +1,16 @@
 -- | The docvim executable.
 module Main (main) where
 
-import Control.Applicative
-  ( (*>)
-  , (<$)
-  , (<$>)
-  , (<*)
-  , (<*>)
-  )
--- TODO: custom error messages with <?>
 import Options (Options(..), options)
+import Parse (parse)
 import ReadDir (readDir)
-import System.Exit (exitFailure)
 import System.FilePath (takeExtension)
-import System.IO (hPutStrLn, stderr)
-import Text.Parsec
-  ( (<|>)
-  , (<?>)
-  , many
-  , many1
-  )
-import Text.Parsec.String (Parser, parseFromFile)
-import Text.Parsec.Combinator (eof)
-import Text.ParserCombinators.Parsec.Char (anyChar, char, noneOf)
-
-data Node = TranslationUnit [Node]
-          | VimScript String
-          | Comment String
-          -- ...everything else
-  deriving (Eq, Show)
-
--- | Parses a translation unit (file contents) into an AST.
-parseUnit :: Parser Node
-parseUnit =   TranslationUnit
-          <$> many parseNode
-          <*  eof
-
-parseNode :: Parser Node
-parseNode =   parseVimScript
-          <|> parseComment
-
--- TODO: for now skip vimscript; later, parse it and use it to enrich the docs
--- or take some manual work out of specifying what's going on
-parseVimScript :: Parser Node
-parseVimScript =   VimScript
-               <$> many1 (noneOf ['"']) -- anyChar
-
-parseComment :: Parser Node
-parseComment =   Comment
-             <$> many1 (char '"')
-
--- parse
-
--- To test in the REPL:
--- Text.Parsec.runParser parseUnit () "this" "that"
-parse :: Parser Node -> String -> IO Node
-parse p fileName = parseFromFile p fileName >>= either report return
-  where
-    report err = do
-      hPutStrLn stderr $ "Error: " ++ show err
-      exitFailure
 
 run :: Options -> IO ()
 run (Options _ _ directory _) = do
   paths <- readDir directory
   let filtered = filter (\path -> takeExtension path == ".vim") paths
-  parsed <- mapM (parse parseUnit) filtered
+  parsed <- mapM parse filtered
   print parsed
   return ()
 
---
+++ src/Parse.hs
@@ -0,0 +1,57 @@
+module Parse (parse) where
+
+import Control.Applicative
+  ( (*>)
+  , (<$)
+  , (<$>)
+  , (<*)
+  , (<*>)
+  )
+import System.Exit (exitFailure)
+import System.FilePath (takeExtension)
+import System.IO (hPutStrLn, stderr)
+-- TODO: custom error messages with <?>
+import Text.Parsec
+  ( (<|>)
+  , (<?>)
+  , many
+  , many1
+  )
+import Text.Parsec.String (Parser, parseFromFile)
+import Text.Parsec.Combinator (eof)
+import Text.ParserCombinators.Parsec.Char (anyChar, char, noneOf)
+
+data Node = TranslationUnit [Node]
+          | VimScript String
+          | Comment String
+          -- ...everything else
+  deriving (Eq, Show)
+
+-- | Parses a translation unit (file contents) into an AST.
+parseUnit :: Parser Node
+parseUnit =   TranslationUnit
+          <$> many parseNode
+          <*  eof
+
+parseNode :: Parser Node
+parseNode =   parseVimScript
+          <|> parseComment
+
+-- TODO: for now skip vimscript; later, parse it and use it to enrich the docs
+-- or take some manual work out of specifying what's going on
+parseVimScript :: Parser Node
+parseVimScript =   VimScript
+               <$> many1 (noneOf ['"']) -- anyChar
+
+parseComment :: Parser Node
+parseComment =   Comment
+             <$> many1 (char '"')
+
+-- To test in the REPL:
+-- Text.Parsec.runParser parseUnit () "this" "that"
+parse :: String -> IO Node
+parse fileName = parseFromFile parseUnit fileName >>= either report return
+  where
+    report err = do
+      hPutStrLn stderr $ "Error: " ++ show err
+      exitFailure
