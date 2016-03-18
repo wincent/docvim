1206ab20f3c1cac44bfdf02582d1cd6feb2d67a8
Greg Hurrell <greg@hurrell.net>
2016-03-17 22:11:09 -0700

    Start build out more AST types

--- src/Main.hs
+++ src/Main.hs
@@ -18,15 +18,29 @@ import ReadDir (readDir)
 import System.Exit (exitFailure)
 import System.FilePath (takeExtension)
 import System.IO (hPutStrLn, stderr)
+import Text.Parsec (many1)
 import Text.Parsec.String (Parser, parseFromFile)
 import Text.ParserCombinators.Parsec.Char (anyChar)
 
-data Node = Node [Char] deriving (Show)
+data Node = TranslationUnit [Node]
+          | VimScript String
+          | Comment String
+          -- ...everything else
+  deriving (Eq, Show)
 
 -- | Parses a translation unit (file contents) into an AST.
 parseUnit :: Parser Node
-parseUnit = Node <$> (many anyChar)
+parseUnit = TranslationUnit
+  <$> (many parseNode)
 
+parseNode :: Parser Node
+parseNode = parseVimScript
+
+parseVimScript :: Parser Node
+parseVimScript = VimScript <$> many1 anyChar
+
+-- To test in the REPL:
+-- Text.Parsec.runParser parseUnit () "this" "that"
 parse :: Parser Node -> String -> IO Node
 parse p fileName = parseFromFile p fileName >>= either report return
   where
@@ -38,8 +52,8 @@ run :: Options -> IO ()
 run (Options _ _ directory _) = do
   paths <- readDir directory
   let filtered = filter (\path -> takeExtension path == ".vim") paths
-  contents <- mapM (parse parseUnit) filtered
-  print contents
+  parsed <- mapM (parse parseUnit) filtered
+  print parsed
   return ()
 
 -- | Run the executable using the supplied options.
