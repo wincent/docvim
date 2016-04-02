42b676cbf081d4834f14ada70b98352fa2de6748
Greg Hurrell <greg@hurrell.net>
2016-04-01 18:40:42 -0700

    Do single walk in symbol visitor

--- lib/Docvim/Visitor/Symbol.hs
+++ lib/Docvim/Visitor/Symbol.hs
@@ -1,5 +1,6 @@
 module Docvim.Visitor.Symbol (getSymbols) where
 
+import Control.Monad (join)
 import Docvim.AST
 
 -- TODO: add auto-gened symbols too:
@@ -10,19 +11,10 @@ getSymbols :: Node -> [String]
 getSymbols node = symbols
   where
     -- TODO: error on duplicates; use a real set
-    symbols = pluginAnnotations ++ linkTargets ++ headings
-
-    -- TODO: merge these into a single walk
-    headings = walk gatherHeadings [] node
-    gatherHeadings nodes (HeadingAnnotation h) = mappend nodes [sanitizeAnchor h]
-    gatherHeadings nodes _ = nodes
-
-    linkTargets = concat $ walk gatherLinkTargets [] node
-    gatherLinkTargets nodes (LinkTargets ts) = mappend nodes [ts]
-    gatherLinkTargets nodes _ = nodes
-
+    symbols                                       = walk gatherSymbols [] node
+    gatherSymbols nodes (HeadingAnnotation h)     = mappend nodes [sanitizeAnchor h]
+    gatherSymbols nodes (LinkTargets ts)          = mappend nodes (join [ts])
     -- TODO: probably don't want this target to exist in the symbol table when
     -- emitting Markdown
-    pluginAnnotations = walk gatherPluginAnnotations [] node
-    gatherPluginAnnotations nodes (PluginAnnotation name _) = mappend nodes [name, name ++ ".txt"]
-    gatherPluginAnnotations nodes _ = nodes
+    gatherSymbols nodes (PluginAnnotation name _) = mappend nodes [name, name ++ ".txt"]
+    gatherSymbols nodes _                         = nodes
