8cd30ab43f6fb9801e67587cc5885abc8d7a4a48
Greg Hurrell <greg@hurrell.net>
2016-04-01 18:56:41 -0700

    Prefix plugin name to title anchors

--- lib/Docvim/Visitor/Symbol.hs
+++ lib/Docvim/Visitor/Symbol.hs
@@ -1,19 +1,31 @@
 module Docvim.Visitor.Symbol (getSymbols) where
 
+import Data.Char (toLower)
 import Docvim.AST
 
--- TODO: add auto-gened symbols too:
---      @plugin foo bar     -> foo and foo.txt
---      # Some heading      -> some-heading
--- (will extract and re-use the sanitizeAnchor logic from the Markdown printer
+-- TODO: error on duplicates; use a real set
 getSymbols :: Node -> [String]
 getSymbols node = symbols
   where
-    -- TODO: error on duplicates; use a real set
     symbols                                       = walk gatherSymbols [] node
-    gatherSymbols nodes (HeadingAnnotation h)     = mappend nodes [sanitizeAnchor h]
+    gatherSymbols nodes (HeadingAnnotation h)     = mappend nodes [titleAnchor h]
     gatherSymbols nodes (LinkTargets ts)          = mappend nodes ts
     -- TODO: probably don't want this target to exist in the symbol table when
     -- emitting Markdown
     gatherSymbols nodes (PluginAnnotation name _) = mappend nodes [name, name ++ ".txt"]
     gatherSymbols nodes _                         = nodes
+    titleAnchor title                             = titlePrefix ++ sanitizeAnchor title
+    titlePrefix                                   = downcase $ maybe "" (++ "-") $ getPluginName node
+
+downcase :: String -> String
+downcase = map toLower
+
+getPluginName :: Node -> Maybe String
+getPluginName node = name
+  where
+    name = if length names == 0
+           then Nothing
+           else Just $ head names
+    names = walk getName [] node
+    getName nodes (PluginAnnotation name _) = mappend nodes [name]
+    getName nodes _                         = nodes
