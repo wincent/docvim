b4cd5d1183a911a955099cb2fd5233efa6fa9a27
Greg Hurrell <greg@hurrell.net>
2016-04-05 22:46:25 -0700

    Start working on Footer visitor
    This one uses the State monad to do a context-sensitive traversal: the
    traversal is either in capturing or non-capturing mode, and it
    accumulates nodes into the final result list on that basis.

---
+++ lib/Docvim/Visitor/Footer.hs
@@ -0,0 +1,43 @@
+module Docvim.Visitor.Footer (getFooter) where
+
+import Control.Monad.State
+import Debug.Trace
+import Docvim.AST
+
+-- | State monad where the state is a Bool indicating whether we are
+-- accumulating nodes after a `@footer` annotation, and the result is a list of
+-- the accumulated nodes.
+type Env = State (Bool, [Node]) [Node]
+
+-- | Gets a list of nodes (if any exist) from the `@footer` section of the
+-- source code.
+--
+-- If multiple footers (potentially across multiple translation units) no
+-- guarantees about order but they just get concatenated in the order we see
+-- them
+getFooter :: Node -> [Node]
+getFooter n = evalState (node n) (False, [])
+
+nodes :: [Node] -> Env
+nodes ns = concat <$> mapM node ns
+
+node :: Node -> Env
+node n = case n of
+  DocBlock d -> do
+    (_, acc) <- get
+    ns <- nodes d
+    put (False, acc) -- Make sure we reset state on exiting docblock.
+    return $ acc ++ ns
+
+  FooterAnnotation -> do
+    (_, acc) <- get
+    put (True, acc)
+    return $ acc
+
+  Unit u -> nodes u
+
+  _ -> do
+    (capture, acc) <- get
+    return $ if capture
+             then acc ++ [n]
+             else acc
