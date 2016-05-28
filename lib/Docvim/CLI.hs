5ec11d14dcad0f3d4a7966e29d029222bd063fdc
Greg Hurrell <greg@hurrell.net>
2016-05-28 07:02:34 -0700

    Teach CLI to operate on target arguments
    Still not writing to filesystem, but will add that in a bit.
    For now, just debugging everything over standard out is
    super convenient.
    
    Obviously, this code is super ugly with that staggered `if`
    statement, so will refactor.

--- lib/Docvim/CLI.hs
+++ lib/Docvim/CLI.hs
@@ -9,9 +9,19 @@ import Docvim.Printer.Markdown (markdown)
 import Docvim.Printer.Vim (vimHelp)
 import Docvim.ReadDir (readDir)
 import System.FilePath (takeExtension)
+import System.IO (hPutStrLn, stderr)
+
+hasExtension :: String -> FilePath -> Bool
+hasExtension ext fp = takeExtension fp == ext
+
+isMarkdown :: FilePath -> Bool
+isMarkdown = hasExtension ".md"
+
+isText :: FilePath -> Bool
+isText = hasExtension ".txt"
 
 isVimScript :: FilePath -> Bool
-isVimScript = (==) ".vim" . takeExtension
+isVimScript = hasExtension ".vim"
 
 run :: IO ()
 run = do
@@ -19,10 +29,25 @@ run = do
   paths <- readDir (directory opts)
   let filtered = filter isVimScript paths
   parsed <- mapM (\path -> do
-      when (verbose opts) (putStrLn $ "Parsing " ++ path)
+      when (verbose opts) (hPutStrLn stderr ("Parsing " ++ path))
       parse path
     ) filtered
   let project = Project parsed
-  let md = markdown project
-  putStrLn md
+  let targets = maybe [""] id (outfiles opts)
+  mapM_ (\target -> do
+      if target == ""
+        then do
+          when (verbose opts) (hPutStrLn stderr ("No output target: defaulting to standard out"))
+          putStrLn $ markdown project
+        else if isText target
+          then do
+            when (verbose opts) (hPutStrLn stderr ("Outputting in text format to " ++ target))
+            putStrLn "text output [not yet implemented]"
+          else if isMarkdown target
+            then do
+              when (verbose opts) (hPutStrLn stderr ("Outputting in markdown format to " ++ target))
+              putStrLn $ markdown project
+            else do
+              hPutStrLn stderr ("Unrecognized output format for " ++ target)
+    ) targets
   return ()
