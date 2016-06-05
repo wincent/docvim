7b1b26b898c8ac70d7add30fdeabf9079527c5cc
Greg Hurrell <greg@hurrell.net>
2016-06-05 00:30:29 -0700

    Extract Visitor module
    First step in factoring out some of the duplication here.

---
+++ lib/Docvim/Visitor.hs
@@ -0,0 +1,31 @@
+module Docvim.Visitor (extract, extractBlocks) where
+
+import Control.Applicative (Alternative, (<|>), empty)
+import Control.Monad ((>=>))
+-- TODO switch to pure mtl here (reduce dependency footprint)
+import Control.Monad.Trans.Writer (runWriter)
+import Data.Data.Lens
+import qualified Data.DList as DList
+
+-- extract :: Node -> (Node, [Node])
+extract extractor = toList . runWriter . postorder uniplate extractor
+  where toList (ast, dlist) = (ast, concat $ DList.toList dlist)
+
+extractBlocks :: Alternative f => (a -> Maybe (a -> Bool)) -> [a] -> (f [a], [a])
+extractBlocks start = go
+  where
+    go     [] = (empty, [])
+    go (x:xs) = maybe no_extract extract (start x)
+      where
+        no_extract = (extracted, x:unextracted)
+          where
+            ~(extracted, unextracted) = go xs
+        extract stop = (pure (x:block) <|> extracted, unextracted)
+          where
+            ~(block, remainder) = break stop xs
+            ~(extracted, unextracted) = go remainder
+
+postorder :: Monad m => ((a -> m c) -> (a -> m b)) -> (b -> m c) -> (a -> m c)
+postorder t f = go
+  where
+    go = t go >=> f
--- lib/Docvim/Visitor/Footer.hs
+++ lib/Docvim/Visitor/Footer.hs
@@ -2,12 +2,11 @@
 
 module Docvim.Visitor.Footer (extract) where
 
-import Control.Applicative (Alternative, (<|>), empty)
-import Control.Monad ((>=>))
+import Control.Applicative (Alternative, (<|>))
 import Control.Monad.Trans.Writer
 import qualified Data.DList as DList
-import Data.Data.Lens
 import Docvim.AST
+import qualified Docvim.Visitor as Visitor
 
 -- | Extracts a list of nodes (if any exist) from the `@footer` section(s) of
 -- the source code.
@@ -16,12 +15,8 @@ import Docvim.AST
 -- footers (potentially across multiple translation units) exist, there are no
 -- guarantees about order but they just get concatenated in the order we see
 -- them.
-extract :: Node -> (Node, [Node])
-extract = toList . runWriter . postorder uniplate extractNodeFooters
-  where toList (ast, dlist) = (ast, concat $ DList.toList dlist)
-
--- TODO: consider making this a pipeline, where we just append the footer to the
--- end of the AST after extraction.
+-- extract :: Node -> (Node, [Node])
+extract = Visitor.extract extractNodeFooters
 
 -- | Returns True if a node marks the end of a @footer region.
 endFooter :: Node -> Bool
@@ -35,7 +30,6 @@ endFooter = \case
     PluginAnnotation {}    -> True
     _                      -> False
 
--- TODO: change the name here; not super happy with it
 extractNodeFooters :: Node -> Writer (DList.DList [Node]) Node
 extractNodeFooters (DocBlock nodes) = do
     let (footers, remainder) = extractFooters nodes
@@ -43,28 +37,9 @@ extractNodeFooters (DocBlock nodes) = do
     return (DocBlock remainder)
 extractNodeFooters node = return node
 
-extractBlocks :: Alternative f => (a -> Maybe (a -> Bool)) -> [a] -> (f [a], [a])
-extractBlocks start = go
-  where
-    go     [] = (empty, [])
-    go (x:xs) = maybe no_extract extract (start x)
-      where
-        no_extract = (extracted, x:unextracted)
-          where
-            ~(extracted, unextracted) = go xs
-        extract stop = (pure (x:block) <|> extracted, unextracted)
-          where
-            ~(block, remainder) = break stop xs
-            ~(extracted, unextracted) = go remainder
-
 extractFooters :: Alternative f => [Node] -> (f [Node], [Node])
-extractFooters = extractBlocks f
+extractFooters = Visitor.extractBlocks f
   where
     f x = if x == FooterAnnotation
           then Just endFooter
           else Nothing
-
-postorder :: Monad m => ((a -> m c) -> (a -> m b)) -> (b -> m c) -> (a -> m c)
-postorder t f = go
-  where
-    go = t go >=> f
