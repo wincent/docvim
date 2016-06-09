36babdaae972c51151d8c5b293e965cb27040e7e
Greg Hurrell <greg@hurrell.net>
2016-06-08 22:03:43 -0700

    Make sure generated headings appear in TOC node

--- lib/Docvim/Visitor/Heading.hs
+++ lib/Docvim/Visitor/Heading.hs
@@ -12,7 +12,11 @@ import Docvim.AST
 getHeadings :: Node -> [String]
 getHeadings = walk gather []
   where
+    gather CommandsAnnotation    = ["Commands"]
+    gather FunctionsAnnotation   = ["Functions"]
     gather (HeadingAnnotation h) = [h]
+    gather MappingsAnnotation    = ["Mappings"]
+    gather OptionsAnnotation     = ["Options"]
     gather _                     = []
 
 -- | Injects a table of contents immediately after any `PluginAnnotation` in an
@@ -20,8 +24,8 @@ getHeadings = walk gather []
 -- TODO: warn or error if there is more than one.
 -- or use a monadic variant of transform to do only the first...
 injectTOC :: Node -> Node
-injectTOC ast = transform inject $ ast
+injectTOC ast = transform inject ast
   where
     inject n = case n of
-      PluginAnnotation {} -> DocBlock $ [n] ++ [TOC $ getHeadings ast]
+      PluginAnnotation {} -> DocBlock $ n : [TOC $ getHeadings ast]
       _                   -> n
