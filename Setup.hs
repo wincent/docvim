621a4d30f17a9fda64cf6b37d4608cbe08bc72e3
Greg Hurrell <greg@hurrell.net>
2020-12-08 21:16:45 +0100

    fix: work around inability to build
    Seeing an issue like this:
    
        https://github.com/commercialhaskell/stack/issues/157
    
    Fix from here:
    
        https://stackoverflow.com/a/39019781
    
    With this, this (ie. building updated Ferret docs) works:
    
        git clone https://github.com/wincent/docvim.git
        cd docvim
        pacman -S stack happy
        bin/docvim \
            -c ~/.vim/pack/bundle/opt/ferret \
            ~/.vim/pack/bundle/opt/ferret/README.md \
            ~/.vim/pack/bundle/opt/ferret/doc/ferret.txt

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,17 @@
 import Distribution.Simple
-main = defaultMain
+import System.Environment
+
+-- Hacking this to work based on:
+--
+--    https://stackoverflow.com/a/39019781
+--
+-- Relies on `happy` existing at /usr/bin/happy; eg. on Arch Linux:
+--
+--    sudo pacman -S stack happy
+
+main = do
+  args <- getArgs
+  let args' = if elem "configure" args
+    then args ++ [ "--with-happy=/usr/bin/happy" ]
+    else args
+  defaultMainWithArgs args'
