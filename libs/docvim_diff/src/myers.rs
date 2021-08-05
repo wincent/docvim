b6233581f7713b6942ebb2c5bda0b91b4746a17e
Greg Hurrell <greg@hurrell.net>
2021-08-05 18:40:54 +0200

    docs: add some more code comments
    Possibly too many. I think this stuff is like monad tutorials; you don't
    really understand it until you've explained it, and nobody can come up
    with a definitive explanation that will obviate this need, so the number
    of explanations proliferate endlessly.
    
    Note the use of "```ignore" to stop Rust doctests from trying to compile
    my diagram as code.

--- libs/docvim_diff/src/myers.rs
+++ libs/docvim_diff/src/myers.rs
@@ -97,10 +97,48 @@ where
     Diff(edits)
 }
 
-/// This is the "O(ND)" time and "O(ND)" space version of the diff algorithm presented in the first
-/// part of the Myers paper. Here "N" is the _sum_ of the lengths of the inputs and "D" is the
-/// length of the minimal edit script, so we could also write the time and space requirement as
-/// "O(ND + MD)".
+/// Finds a Shortest Edit Script to transform `a` into `b`. This is equivalent to finding the
+/// shortest path through the edit graph from the origin `(0, 0)` at the top-right to the target
+/// `(n, m)` at the bottom left. In an edit graph, horizontal edges represent a deletion from `a`,
+/// vertical edges represent insertion from `b`, and diagonal edges indicate match points between
+/// `a` and `b` where no edit is required. In the following diagram, `*` shows a shortest path from
+/// input `a` ("ABCABBA") to `b` ("CBABAC"):
+///
+///
+/// ```ignore
+///               (0, 0)
+///                    \
+///                     #*A*B-C-A-B-B-A 0                   SES (Shortest Edit Script):
+///                     ¦ ¦ ¦*¦ ¦ ¦ ¦ ¦                       Delete index 1 from `a`
+///                     C-¦-¦-*-¦-¦-¦-¦ 1                     Delete index 2 from `a`
+///                     ¦ ¦ ¦ * ¦ ¦ ¦ ¦                       Insert index 2 from `b`
+///                     B-¦-¦-*-¦-¦-¦-¦ 2                     Delete index 6 from `a`
+///                     ¦ ¦ ¦ ¦*¦ ¦ ¦ ¦                       Insert index 6 from `b`
+///                     A-¦-¦-¦-*-¦-¦-¦ 3
+///                     ¦ ¦ ¦ ¦ ¦*¦ ¦ ¦                     LCS (Longest Common Subsequence):
+///                     B-¦-¦-¦-¦-***-¦ 4                     C, A, B, A
+///                     ¦ ¦ ¦ ¦ ¦ ¦ ¦*¦
+///                     A-¦-¦-¦-¦-¦-¦-* 5
+///                     ¦ ¦ ¦ ¦ ¦ ¦ ¦ *
+///                     C-¦-¦-¦-¦-¦-¦-# 6
+///                                    \
+///                     0 1 2 3 4 5 6 7 \
+///                                     (7, 6)
+/// ```
+///
+/// The SES corresponds to the horizontal and vertical edges. The LCS corresponds to the diagonal
+/// edges. In the Myers paper runs of 0 or more diagonal edges are called "snakes". Note that there
+/// may be more than one SES, but the algorithm is greedy and favors deletions, so it returns the
+/// SES shown above.
+///
+/// This is the `O(ND)` time and `O(ND)` space version of the diff algorithm presented in the
+/// first part of the Myers paper. Note that `N` here is the _sum_ of the lengths of the inputs
+/// and "D" is the length of the minimal edit script, so, following the convention used elsewhere
+/// in the paper (where `N` and `M` refer to the input lengths), we could also write the time and
+/// space requirement as `D(N + M)`. Given that the upper bound for `D` is `N + M`, we have have
+/// quadratic growth which makes this algorithm impractical for anything but the most modest input
+/// sizes.
+///
 pub fn myers_nd_diff<T>(a: &T, a_range: Range<usize>, b: &T, b_range: Range<usize>) -> Diff
 where
     T: Index<usize> + ?Sized,
@@ -112,7 +150,34 @@ where
     let mut v = RingBuffer::new(max * 2 + 1); // Range from -max to +max, with extra slot for 0.
     let mut vs = vec![];
     v[1] = 0;
+
+    // Find all "D-paths" between the origin (0, 0) and the destination (n, m). A D-path represents
+    // all the locations that can be reached in the edit graph after D edits. The algorithm is
+    // greedy, so the first path that reaches (n, m) terminates.
+    //
+    // The best case scenario is a graph where (n, m) can be reached via a 0-path; that is, where
+    // no edits are needed to transform input `a` into input `b` (eg. "abcd" to "abcd"). This
+    // corresponds to a path where all edges are diagonal.
+    //
+    // The worst case scenario is where an "n+m" path is required to reach the destination; this
+    // occurs when every item in `a` must be deleted, then every item in `b` inserted (eg. "abcd"
+    // to "wxyz"). This corresponds to a path where no edges are diagonal.
+    //
+    // For typical inputs, there will be multiple D-paths corresponding to the different locations
+    // in the graph that can be reached after 0, 1, 2 .. `max` edits, but the first D-path that
+    // reaches the destination will terminate the algorithm.
     for d in 0..=(usize_to_isize(max)) {
+        // We step by 2 because for any given k-line, the paths that reach that k-line originate
+        // from `k - 1` or `k + 1`. That is, a path ending on, say, `k = 3` must consist of either:
+        //
+        // - A horizontal (insertion) edge coming from `k - 1` (2) followed by a (possibly
+        //   zero-length) snake; or:
+        // - A vertical (deletion) edge coming from `k + 1` (4) followed by a (possibly
+        //   zero-length) snake.
+        //
+        // Thus, when `d` is even, we end up writing new values for even k-lines, and reading
+        // values from odd k-lines; when `d` is odd, we write new vales for odd k-lines, and read
+        // values from even k-lines.
         for k in (-d..=d).step_by(2) {
             let mut x: usize;
             let mut y: usize;
