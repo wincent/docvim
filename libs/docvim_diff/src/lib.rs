f076790e6caeeff7f800d047c814d6f9fa109946
Greg Hurrell <greg@hurrell.net>
2021-08-15 11:58:35 +0200

    feat: add first pass at printing proper context and hunk headers
    As noted, I almost certainly have an off-by-one (or more) in here, but
    this produces reasonable output for our histogram test case diff. Going
    to add more tests, obviously, and fix any bugs that I find, in future
    commits. Also as noted inline, probably want to extract this into
    another file.
    
    And then once all that is done, look at refactorings. For now I've
    written it "dumb and verbose" just to see it working, but there may be
    patterns in here that can be used to reduce the duplication.

--- libs/docvim_diff/src/lib.rs
+++ libs/docvim_diff/src/lib.rs
@@ -3,55 +3,157 @@ pub mod histogram;
 pub mod myers;
 mod ring_buffer;
 
+use std::cmp::min;
+use std::fmt::Display;
+
 use crate::diff::*;
 use Edit::*;
 
 const CONTEXT_LINES: usize = 3;
+const CYAN: &str = "\x1b[0;36m";
 const GREEN: &str = "\x1b[0;32m";
 const RED: &str = "\x1b[0;31m";
 const RESET: &str = "\x1b[0m";
 
+// TODO: move this out into a different file
+
 pub fn format_es<T>(ses: Diff, a: &Vec<T>, b: &Vec<T>) -> String
 where
-    T: std::fmt::Display + PartialEq,
+    T: Display + PartialEq,
 {
     let mut diff = String::new();
+    let mut hunk = String::new();
+    let mut a_idx = 1;
+    let mut b_idx = 1;
+    let mut a_hunk_start = None;
+    let mut b_hunk_start = None;
+    let a_len = a.len();
+    let b_len = b.len();
 
-    let mut a_idx = 1; // Where we are in `a`.
-    let mut b_idx = 1; // Where we are in `b`.
-
-    // TODO: print hunk headers; eg. @@ -44,6 +47,7 @@
     for edit in ses.0 {
         match edit {
             Delete(Idx(delete)) => {
-                while a_idx < delete {
-                    if delete < CONTEXT_LINES || a_idx >= delete - CONTEXT_LINES {
-                        diff.push_str(&format!(" {}\n", &a[a_idx - 1]));
+                if delete > a_idx + CONTEXT_LINES {
+                    // Print trailing context.
+                    let last_context_line = min(a_idx + CONTEXT_LINES, a_len);
+                    while a_idx <= last_context_line {
+                        hunk.push_str(&format!(" {}\n", &a[a_idx - 1]));
+                        a_idx += 1;
+                        b_idx += 1;
                     }
-                    a_idx += 1;
-                    b_idx += 1;
+                    // BUG: almost certainly have off-by-ones in here
+                    diff.push_str(&format!(
+                        "{}@@ -{},{} +{},{} @@{}\n",
+                        CYAN,
+                        a_hunk_start.unwrap(),
+                        a_idx - a_hunk_start.unwrap(),
+                        b_hunk_start.unwrap(),
+                        b_idx - b_hunk_start.unwrap(),
+                        RESET
+                    ));
+                    diff.push_str(&hunk);
+                    hunk.clear();
+                    a_hunk_start = None;
+                    b_hunk_start = None;
                 }
-                diff.push_str(&format!("{}-{}{}\n", RED, &a[a_idx - 1], RESET));
+
+                if a_idx < delete {
+                    // Print leading context.
+                    if a_hunk_start.is_none() {
+                        if delete <= CONTEXT_LINES {
+                            a_hunk_start = Some(1);
+                            b_hunk_start = Some(1);
+                        } else if a_idx >= delete - CONTEXT_LINES {
+                            a_hunk_start = Some(a_idx);
+                            b_hunk_start = Some(b_idx);
+                        } else {
+                            let skip = (delete - CONTEXT_LINES) - a_idx;
+                            a_hunk_start = Some(a_idx + skip);
+                            b_hunk_start = Some(b_idx + skip);
+                        }
+                    }
+                    while a_idx < delete {
+                        if delete < CONTEXT_LINES || a_idx >= delete - CONTEXT_LINES {
+                            hunk.push_str(&format!(" {}\n", &a[a_idx - 1]));
+                        }
+                        a_idx += 1;
+                        b_idx += 1;
+                    }
+                }
+                hunk.push_str(&format!("{}-{}{}\n", RED, &a[a_idx - 1], RESET));
                 a_idx += 1;
             }
             Insert(Idx(insert)) => {
-                while b_idx < insert {
-                    if insert < CONTEXT_LINES || b_idx >= insert - CONTEXT_LINES {
-                        diff.push_str(&format!(" {}\n", &b[b_idx - 1]));
+                if insert > b_idx + CONTEXT_LINES {
+                    // Print trailing context.
+                    let last_context_line = min(b_idx + CONTEXT_LINES, b_len);
+                    while b_idx <= last_context_line {
+                        hunk.push_str(&format!(" {}\n", &b[b_idx - 1]));
+                        a_idx += 1;
+                        b_idx += 1;
                     }
-                    a_idx += 1;
-                    b_idx += 1;
+                    diff.push_str(&format!(
+                        "{}@@ -{},{} +{},{} @@{}\n",
+                        CYAN,
+                        a_hunk_start.unwrap(),
+                        a_idx - a_hunk_start.unwrap(),
+                        b_hunk_start.unwrap(),
+                        b_idx - b_hunk_start.unwrap(),
+                        RESET
+                    ));
+                    diff.push_str(&hunk);
+                    hunk.clear();
+                    a_hunk_start = None;
+                    b_hunk_start = None;
                 }
-                diff.push_str(&format!("{}+{}{}\n", GREEN, &b[b_idx - 1], RESET));
+
+                if b_idx < insert {
+                    // Print leading context.
+                    if b_hunk_start.is_none() {
+                        if insert <= CONTEXT_LINES {
+                            a_hunk_start = Some(1);
+                            b_hunk_start = Some(1);
+                        } else if b_idx >= insert - CONTEXT_LINES {
+                            a_hunk_start = Some(a_idx);
+                            b_hunk_start = Some(b_idx);
+                        } else {
+                            let skip = (insert - CONTEXT_LINES) - b_idx;
+                            a_hunk_start = Some(a_idx + skip);
+                            b_hunk_start = Some(b_idx + skip);
+                        }
+                    }
+                    while b_idx < insert {
+                        if insert < CONTEXT_LINES || b_idx >= insert - CONTEXT_LINES {
+                            hunk.push_str(&format!(" {}\n", &b[b_idx - 1]));
+                        }
+                        a_idx += 1;
+                        b_idx += 1;
+                    }
+                }
+                hunk.push_str(&format!("{}+{}{}\n", GREEN, &b[b_idx - 1], RESET));
                 b_idx += 1;
             }
         }
-        // TODO: after each edit, may want to print up to three lines of context...
     }
-
-    // BUG: there is almost certainly an off-by-one or a boundary violation hiding (ha!) in here
-    for i in (a_idx - 1)..=a.len() {
-        diff.push_str(&format!(" {}\n", &a[i - 1]));
+    // Flush any pending hunks.
+    if hunk.len() > 0 {
+        // Append trailing context lines before flushing.
+        let last_context_line = min(a_idx + CONTEXT_LINES, a_len);
+        while a_idx <= last_context_line {
+            hunk.push_str(&format!(" {}\n", &a[a_idx - 1]));
+            a_idx += 1;
+            b_idx += 1;
+        }
+        diff.push_str(&format!(
+            "{}@@ -{},{} +{},{} @@{}\n",
+            CYAN,
+            a_hunk_start.unwrap(),
+            a_idx - a_hunk_start.unwrap(),
+            b_hunk_start.unwrap(),
+            b_idx - b_hunk_start.unwrap(),
+            RESET
+        ));
+        diff.push_str(&hunk);
     }
 
     diff
