384de045c1d5d996a1d33e27601a4df44d661e11
Greg Hurrell <greg@hurrell.net>
2021-08-16 16:35:55 +0200

    test: add snapshot test for a complex expression
    This mirrors the test I had already written out by hand, but will be
    easier to maintain.

---
+++ libs/docvim_parser/tests/snapshots.rs
@@ -0,0 +1,16 @@
+use std::error::Error;
+
+use docvim_parser::lua::Parser;
+use docvim_snapshot::check_snapshot;
+
+fn transform(input: &str) -> String {
+    let mut parser = Parser::new(input);
+    let ast = parser.parse();
+    format!("{:#?}", ast)
+}
+
+#[test]
+fn test_parses_a_complex_expression() -> Result<(), Box<dyn Error>> {
+    assert!(check_snapshot!("complex_expression", &transform)?);
+    Ok(())
+}
